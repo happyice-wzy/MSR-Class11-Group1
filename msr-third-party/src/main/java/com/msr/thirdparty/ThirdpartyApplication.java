3141f538e0d370c8ecf5dd282e5ec58bb5696959
wzy <wzyrb1882@gmail.com>
2020-07-12 21:54:15 +0900

    first create

---
+++ msr-third-party/src/main/java/com/msr/thirdparty/ThirdpartyApplication.java
@@ -0,0 +1,13 @@
+package com.msr.thirdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ThirdpartyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdpartyApplication.class,args);
+    }
+}
